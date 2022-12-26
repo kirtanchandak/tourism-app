6c37948046876657fb9351731573e49ae282d140
kirtanchandak <themarketingkirtan@gmail.com>
2022-12-26 21:12:49 +0530

    footer build

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import { Inter } from "@next/font/google";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
@@ -15,6 +16,7 @@ export default function Home() {
       <div className="bg-gray-800">
         <Header />
         <Hero />
+        <Footer />
       </div>
     </>
   );
